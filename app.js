08d4e11a20e08b33e983a103df7ea76d09920400
Varun Joshi <joshvarun@gmail.com>
2019-01-29 12:12:04 +0530

    Add rules

--- app.js
+++ app.js
@@ -59,6 +59,14 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
 
 document.querySelector('.btn-new').addEventListener('click', init);
 
+
+document.querySelector('.btn-rules').addEventListener('click', function () {
+    var rules;
+    rules = "GAME RULES: \n- The game has 2 players, playing in rounds \n- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score \n- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn \n- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn \n- The first player to reach 100 points on GLOBAL score wins the game"
+
+    alert(rules);
+});
+
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
